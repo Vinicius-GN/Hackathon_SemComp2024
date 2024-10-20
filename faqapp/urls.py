cd524c35be5a9af608476065a23a0f1ba5096edc
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2024-10-20 11:59:43 -0400

    branch de deploy limpa

--- faqapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views 
-
-urlpatterns = [
-    path("", views.index, name="index"), 
-    path("categoria/<str:nome_categoria>", views.categoria, name = "categoria"),
-    path("formulario/", views.formulario, name = "formulario"),
-    path("teste", views.busca_categoria, name="busca_categoria")
-]
